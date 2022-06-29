ef6349b58cb0d6ce14bcd3332de4641293f93f4e
Yaroslav <pablo1231435@gmail.com>
2022-06-29 19:51:37 +0200

    check

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 <ins> Template </ins> -- is a function. input -> **Template** -> output (text of the template)
 
-/# - variable in the template
+[Project diagrams](https://lucid.app/lucidchart/cbdb698c-a110-4762-ab0f-9bbdb1eed1e0/edit?viewport_loc=-10%2C-10%2C1707%2C821%2C0_0&invitationId=inv_f1f63ead-021f-48e5-93a5-743bb9337ae8#)
+\# - variable in the template
 
 # Ats(dogs)(tags):
 
---
+++ src/TemplateProcessor.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Templator
+{
+    public class TemplateProcessor
+    {
+        // should it have side effects? or not? Yes, probably/
+        // also I want it to be a tree
+        public void Process()
+        {
+            // process all tags
+            // register the selector
+            // register variables
+        }
+    }
+}
